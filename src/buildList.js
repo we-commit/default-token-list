b6cf9dd02bb6af52397f1dcd6244c6b676967cd7
1devNdogs <vsalgado.a@gmil.com>
2021-03-14 20:36:56 -0300

    update to trojan allowed tokens

--- src/buildList.js
+++ src/buildList.js
@@ -1,14 +1,10 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Default List",
+    name: "Trojan Default List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -17,8 +13,8 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    keywords: ["trojan", "default"],
+    tokens: [...mainnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
